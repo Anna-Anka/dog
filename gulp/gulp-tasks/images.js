0b57c8c0ed8440301c919a30e58b2738538600d3
Anna-Anka <anjbekrenewa@gmail.com>
2023-12-22 20:56:32 +0300

    corect gallery and images gulp

--- gulp/gulp-tasks/images.js
+++ gulp/gulp-tasks/images.js
@@ -1,13 +1,10 @@
 import { paths } from '../../gulpfile.babel';
 import { plugins } from '../plugins';
 
-const { argv } = plugins.yargs;
-const production = !!argv.production;
-
 plugins.gulp.task('images', () => {
     const result = plugins.gulp.src(paths.images.src)
-        .pipe(plugins.gulpif(!production, plugins.newer(paths.images.app)))
-        .pipe(plugins.gulpif(production, plugins.imagemin([
+        .pipe(plugins.newer(paths.images.app))
+        .pipe(plugins.imagemin([
             plugins.imageminGiflossy({
                 optimizationLevel: 3,
                 optimize: 3,
@@ -22,7 +19,7 @@ plugins.gulp.task('images', () => {
             }),
             plugins.imageminMozjpeg({
                 progressive: true,
-                quality: 90,
+                quality: 95,
             }),
             plugins.imagemin.svgo({
                 plugins: [
@@ -36,7 +33,7 @@ plugins.gulp.task('images', () => {
                     { collapseGroups: true },
                 ],
             }),
-        ])))
+        ]))
         .pipe(plugins.gulp.dest(paths.images.app))
         .pipe(plugins.debug({
             title: 'Images',
