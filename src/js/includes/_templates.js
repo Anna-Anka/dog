1555360cc3da42c3b0970dcbe66f6acbd7e2393f
Anna-Anka <anjbekrenewa@gmail.com>
2024-01-12 19:02:12 +0300

    add scroll smoth

--- src/js/includes/_templates.js
+++ src/js/includes/_templates.js
@@ -1,5 +1,7 @@
 import { burger } from '../templates/_burger';
 import { headerSticky } from '../templates/_header-sticky';
+import { pageNavigation } from '../templates/_page-navigation';
 
 burger();
 headerSticky();
+pageNavigation();
